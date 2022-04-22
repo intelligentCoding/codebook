92b61971a34c8de2fc74c2250df336af0aa6fd43
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2022-04-22 16:51:40 -0600

    use path to get actual path

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,9 +1,12 @@
+import path from 'path';
 import  { Command } from 'commander';
 import { serve } from 'local-api';
 export const serveCommand = new Command()
     .command('serve [fileName]')
     .description('Open a file for editing')
     .option('-p, --port <number>', 'port to run a server on', '4005')
-    .action((fileName = 'notebook.js', options: {port: string}) => {
-        serve(parseInt(options.port), fileName, '/')
+    .action((filename = 'notebook.js', options: {port: string}) => {
+        const dir = path.join(process.cwd(), path.dirname(filename));
+
+        serve(parseInt(options.port), path.basename(filename), dir)
     })
