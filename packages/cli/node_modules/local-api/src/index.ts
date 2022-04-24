19ecfb317d5492b70a2d8c5dd42675c75dff6365
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2022-04-24 06:20:22 -0600

    proxy middleware setup

--- packages/cli/node_modules/local-api/src/index.ts
+++ packages/cli/node_modules/local-api/src/index.ts
@@ -1,6 +1,13 @@
 import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 export const serve = (port: number, filename: string, dir: string) => {
     const app = express();
+    app.use(express.static(''))
+    app.use(createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent'
+    }));
 
     return new Promise<void>((resolve, reject) => {
         app.listen(port, resolve).on('error', reject);
